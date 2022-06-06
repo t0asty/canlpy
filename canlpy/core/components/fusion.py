42cbebdfeb13d52488252e7d59808a506ebe66b5
Squalene <anamaste@mail.com>
2022-06-06 11:03:01 +0200

    Add Fusion subclass for ERNIE

---
+++ canlpy/core/components/fusion.py
@@ -0,0 +1,42 @@
+import torch.nn as nn
+from canlpy.core.models.common.activation_functions import get_activation_function
+from canlpy.core.models.bert.model import DenseSkipLayer
+
+class Fusion(nn.Module):
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self,token_embeddings,entity_embeddings):
+        """
+        Performs the information fusion between the token and entity emmbeddings
+        Args:
+            token_embeddings: the tokens embeddings
+            entity_embeddings: the entity embeddings
+        Returns:
+            token_embeddings: the enhanced embeddings of the tokens 
+            entity embeddings(Optional): the enhanced embeddings of the entities
+        """
+        raise NotImplementedError()
+
+
+class ErnieFusion(Fusion):
+    def __init__(self, hidden_size,entity_size,intermediate_size,hidden_dropout_prob,activation_fn):
+        super().__init__()
+
+        self.dense_intermediate_tokens = nn.Linear(hidden_size, intermediate_size)
+        self.dense_intermediate_ent = nn.Linear(entity_size, intermediate_size)
+        self.intermediate_act_fn = get_activation_function(activation_fn) if isinstance(activation_fn, str) else activation_fn
+
+        self.skip_layer_tokens = DenseSkipLayer(intermediate_size,hidden_size,hidden_dropout_prob)
+        self.skip_layer_ent = DenseSkipLayer(intermediate_size,entity_size,hidden_dropout_prob)
+
+    def forward(self, attention_tokens, attention_ent):
+        
+        intermediate_tokens = self.dense_intermediate_tokens(attention_tokens)
+        intermediate_ent = self.dense_intermediate_ent(attention_ent)
+        intermediate_hidden = self.intermediate_act_fn(intermediate_tokens+intermediate_ent)
+
+        hidden_states = self.skip_layer_tokens(intermediate_hidden,attention_tokens)
+        hidden_states_ent = self.skip_layer_ent(intermediate_hidden,attention_ent)
+
+        return hidden_states,hidden_states_ent
--- canlpy/core/models/ernie/model.py
+++ canlpy/core/models/ernie/model.py
@@ -12,6 +12,7 @@ import logging
 import canlpy.core.models.bert.model as bert
 from canlpy.core.models.bert.model import BertLayer, DenseSkipLayer, BertAttention, BertEmbeddings, BertPooler, LayerNorm
 from canlpy.core.models.common.activation_functions import get_activation_function
+from canlpy.core.components.fusion import ErnieFusion
 
 
 logger = logging.getLogger(__name__)
@@ -99,32 +100,6 @@ class ErnieConfig(object):
         """Serializes this instance to a Python dictionary."""
         output = copy.deepcopy(self.__dict__)
         return output
-
-class ErnieFusion(nn.Module):
-    def __init__(self, hidden_size,entity_size,intermediate_size,hidden_dropout_prob,activation_fn):
-        super().__init__()
-
-        #BertIntermediate
-        self.dense_intermediate_tokens = nn.Linear(hidden_size, intermediate_size)
-        self.dense_intermediate_ent = nn.Linear(entity_size, intermediate_size)
-        self.intermediate_act_fn = get_activation_function(activation_fn) if isinstance(activation_fn, str) else activation_fn
-
-        #BertOutput
-        self.skip_layer_tokens = DenseSkipLayer(intermediate_size,hidden_size,hidden_dropout_prob)
-        self.skip_layer_ent = DenseSkipLayer(intermediate_size,entity_size,hidden_dropout_prob)
-
-    def forward(self, attention_tokens, attention_ent):
-        
-        #BertIntermediate
-        intermediate_tokens = self.dense_intermediate_tokens(attention_tokens)
-        intermediate_ent = self.dense_intermediate_ent(attention_ent)
-        intermediate_hidden = self.intermediate_act_fn(intermediate_tokens+intermediate_ent)
-
-        #BertOutput
-        hidden_states = self.skip_layer_tokens(intermediate_hidden,attention_tokens)
-        hidden_states_ent = self.skip_layer_ent(intermediate_hidden,attention_ent)
-
-        return hidden_states,hidden_states_ent
 
 class ErnieLayer(nn.Module):
     def __init__(self, hidden_size,entity_size,intermediate_size,num_attention_heads,num_attention_heads_ent,attention_probs_dropout_prob,hidden_dropout_prob,activation_fn):
