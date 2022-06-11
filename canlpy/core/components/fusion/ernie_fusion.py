0469d552138b6b42ab7857c6d9ebf092f5565855
julianschnitzler <32015957+julianschnitzler@users.noreply.github.com>
2022-06-11 19:45:52 +0200

    docstrings for ernie_fusion

--- canlpy/core/components/fusion/ernie_fusion.py
+++ canlpy/core/components/fusion/ernie_fusion.py
@@ -5,7 +5,19 @@ from canlpy.core.components.fusion import Fusion
 
 
 class ErnieFusion(Fusion):
+    """ Class for the Fusion of Text and Knowledge representations in ERNIE
+
+    """
     def __init__(self, hidden_size,entity_size,intermediate_size,hidden_dropout_prob,activation_fn):
+        """ Constructs an `ErnieFusion` module
+        
+        Args:
+            hidden_size (int): size of the Text representations
+            entity_size (int): size of the Knowledge representations
+            intermediate_size (int): hidden size of the Fusion layer
+            hidden_dropout_prob (float): dropout probability
+            activation_fn: str or function: activation function of Fusion layer
+        """
         super().__init__()
 
         self.dense_intermediate_tokens = nn.Linear(hidden_size, intermediate_size)
@@ -16,7 +28,16 @@ class ErnieFusion(Fusion):
         self.skip_layer_ent = DenseSkipLayer(intermediate_size,entity_size,hidden_dropout_prob)
 
     def forward(self, attention_tokens, attention_ent):
+        """ Forward pass through the model
         
+        Args:
+            attention_tokens: text representations, a torch.FloatTensor of shape [batch_size, sequence_length, hidden_size]
+            attention_ent: entity representations, a torch.FloatTensor of shape [batch_size, sequence_length, entity_size]
+
+        Returns:
+            hidden_states: New text representations, a torch.FloatTensor of shape [batch_size, sequence_length, hidden_size]
+            hidden_states_ent: New entity representations, a torch.FloatTensor of shape [batch_size, sequence_length, entity_size]
+        """
         intermediate_tokens = self.dense_intermediate_tokens(attention_tokens)
         intermediate_ent = self.dense_intermediate_ent(attention_ent)
         intermediate_hidden = self.intermediate_act_fn(intermediate_tokens+intermediate_ent)
